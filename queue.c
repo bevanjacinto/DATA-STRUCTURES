e761b901a547c9344b9c2d9b71303cabe2259ec8
bevanjacinto <30578613+bevanjacinto@users.noreply.github.com>
2017-08-01 01:36:34 +0530

    Update queue.c

--- queue.c
+++ queue.c
@@ -4,43 +4,8 @@ struct queue
 {
 	int front;
 	int rear;
-	char a[10];
+	int a[10];
 }q;
-
-void insert(struct queue*,char);
-char remove(struct queue *);
-void display(struct queue *);
-void main()
-{
-	char x;
-	int y;
-	q.front=-1;
-    q.rear=-1;
-    while(1)
-    {
-	
-	printf("1.insert\n2.remove\n3.display\n4.exit\n");
-    scanf("%d",&y);
-    switch(y)
-    {
-    	case 1:printf("enter element to be inserted");
-    	       scanf("%c",x);
-    	       insert(&q,x);
-    	       break;
-        case 2:
-               break;
-        case 3:display(&q);
-               break;
-        case 4:break;
-	}
-
-}
-
-
-}
-
-
-
 
 void insert(struct queue *q,char ele)
 {
@@ -63,19 +28,20 @@ void insert(struct queue *q,char ele)
 	}
 	q->a[q->front]=ele;
 }
+	q->rear++;
 }
 
-void remove(struct queue *q)
+void take(struct queue *q)
 {
 
-	char t;
+	int t;
 	if(q->rear==-1 && q->front==-1)
 	printf("queue is empty");
 	else
 {
 		t=q->a[q->rear];
 	q->rear--;
-	printf("removed element is %c\n",t);
+	printf("removed element is %d\n",t);
 }
 }
 
@@ -84,7 +50,36 @@ void display(struct queue *q)
 	int i;
 	for(i=q->rear;i>=0;i--)
 	{
-		printf("%c\n",q->a[i]);
+		printf("%d\n",q->a[i]);
 	}
 }
 
+
+void main()
+{
+	int x;
+	int y;
+	q.front=-1;
+    q.rear=-1;
+    while(1)
+    {
+	
+	printf("1.insert\n2.remove\n3.display\n4.exit\n");
+    scanf("%d",&y);
+    switch(y)
+    {
+    	case 1:printf("enter element to be inserted");
+    	       scanf("%d",&x);
+    	       insert(&q,x);
+    	       break;
+        case 2:take(&q);
+               break;
+        case 3:display(&q);
+               break;
+        case 4:break;
+	}
+
+}
+
+
+}
